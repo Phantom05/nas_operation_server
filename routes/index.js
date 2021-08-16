96f36c675d1873d2db2d1483236494dde74d7b35
phantom05 <monster2jy@gmail.com>
2021-08-17 00:16:51 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,38 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('template/index', { title: 'Express' });
+  // res.render('template/index', { title: 'Operation Server' });
+  res.render('index', { title: 'Operation Server' });
+});
+
+
+router.get('/contact', function(req, res, next) {
+  // res.render('template/index', { title: 'Operation Server' });
+  res.render('contact', { title: 'Operation Server' });
+});
+
+
+router.get('/contact', function(req, res, next) {
+  // res.render('template/index', { title: 'Operation Server' });
+  res.render('contact', { title: 'Operation Server' });
+});
+
+router.get('/signup', function(req, res, next) {
+  // res.render('template/index', { title: 'Operation Server' });
+  res.render('signup', { title: 'Operation Server' });
+});
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin', { title: 'Operation Server' });
+});
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq', { title: 'Operation Server' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Operation Server' });
 });
 
 module.exports = router;
+
